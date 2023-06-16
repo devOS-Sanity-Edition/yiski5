adb97ce4db5e593f8177ae25b0c34cd39af2d52d
Storm Anderson <storm@artrinix.xyz>
2023-06-17 04:20:58 +1200

    ⬆️ Should update the rest of these too

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    kotlin("jvm") version "1.7.20"
-    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("jvm") version "1.8.21"
+    kotlin("plugin.serialization") version "1.8.21"
     id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
@@ -17,19 +17,20 @@ repositories {
 
 dependencies {
     // Logger libraries for writing to the console
-    api("org.slf4j:slf4j-api:2.0.3")
-    implementation("ch.qos.logback:logback-classic:1.4.4")
-    implementation("ch.qos.logback:logback-core:1.4.4")
+    api("org.slf4j:slf4j-api:2.0.7")
+    implementation("ch.qos.logback:logback-classic:1.4.8")
+    implementation("ch.qos.logback:logback-core:1.4.8")
     // Kotlin
     implementation(kotlin("stdlib-common"))
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:1.7.1")
     // JSON Serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
     // TOML Serialization - YW Devin
-    implementation("com.akuleshov7:ktoml-core:0.3.0")
-    implementation("com.akuleshov7:ktoml-file:0.3.0")
+    implementation("com.akuleshov7:ktoml-core:0.5.0")
+    implementation("com.akuleshov7:ktoml-source:0.5.0")
+    implementation("com.akuleshov7:ktoml-file:0.5.0")
     // JDA
     implementation("com.github.DV8FromtheWorld:JDA:v5.0.0-beta.10") {
         exclude(module = "opus-java")
@@ -41,7 +42,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         jvmTarget = "17"
-        freeCompilerArgs = listOf("-Xinline-classes", "-Xopt-in=kotlin.RequiresOptIn")
+        freeCompilerArgs = listOf("-Xinline-classes", "-opt-in=kotlin.RequiresOptIn")
     }
 }
 
